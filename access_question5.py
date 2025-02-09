db9156b6be8d2f459ab656f5482cc3425b9fadc2
Anthony <64691870+anthonyhuang19@users.noreply.github.com>
2025-02-09 18:19:50 +0700

    Update and rename question_5.py to access_question5.py

--- access_question5.py
+++ access_question5.py
@@ -5,7 +5,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="864503",
+    password="password",
     database="OrderDB"
 )
 cursor = mydb.cursor()
